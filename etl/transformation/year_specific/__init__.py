78e8c37b9f005e03fcccf73c27867c20fa1e7cce
morsoletodev <106322393+morsoletodev@users.noreply.github.com>
2025-05-18 16:46:12 -0300

    refactor: rename function to handle_year

--- etl/transformation/year_specific/__init__.py
+++ etl/transformation/year_specific/__init__.py
@@ -4,9 +4,8 @@ import pandas as pd
 import etl.transformation.year_specific.year_2012
 
 
-def apply_year_specific_changes (url: str,
-                                 year: int) -> pd.DataFrame:
+def handle_year (path: str, year: int) -> pd.DataFrame:
     package = f'etl.transformation.year_specific.year_{year}'
     handler = getattr(importlib.import_module(package),
                       f'Handler{year}')
-    return handler(url).pipeline()
+    return handler(path).pipeline()
